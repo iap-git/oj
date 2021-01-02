84ee4da830d91cf7f817fabd6940d8b811e3e04e
iap-git <alien.bigfool@gmail.com>
2021-01-03 00:43:18 +0800

    update on Jan 2nd 2021, flip a tree to see if matches.

--- src/main/java/com/fqyuan/leetcode/tree/FlipTree.java
+++ src/main/java/com/fqyuan/leetcode/tree/FlipTree.java
@@ -5,24 +5,30 @@ import java.util.Arrays;
 import java.util.List;
 
 public class FlipTree {
-    int i = 0;
-    List<Integer> result = new ArrayList<>();
-    public List<Integer> flipMatchVoyage(TreeNode root, int[] voyage) {
-        return dfs(root, voyage)?result: Arrays.asList(-1);
-    }
+  int i = 0;
+  List<Integer> result = new ArrayList<>();
+  /**
+   * https://leetcode.com/problems/flip-binary-tree-to-match-preorder-traversal/submissions/
+   *
+   * @param root
+   * @param voyage
+   * @return
+   */
+  public List<Integer> flipMatchVoyage(TreeNode root, int[] voyage) {
+    return dfs(root, voyage) ? result : Arrays.asList(-1);
+  }
 
-    private boolean dfs(TreeNode node, int[] voyage){
-        if(node == null){
-            return true;
-        }
-        if(node.val != voyage[i++]){
-            return false;
-        }
-        if(node.left !=null && node.left.val != voyage[i]){
-            result.add(node.val);
-            return dfs(node.right, voyage) && dfs(node.left, voyage);
-        }
-        return dfs(node.left, voyage) && dfs(node.right, voyage);
+  private boolean dfs(TreeNode node, int[] voyage) {
+    if (node == null) {
+      return true;
     }
-
+    if (node.val != voyage[i++]) {
+      return false;
+    }
+    if (node.left != null && node.left.val != voyage[i]) {
+      result.add(node.val);
+      return dfs(node.right, voyage) && dfs(node.left, voyage);
+    }
+    return dfs(node.left, voyage) && dfs(node.right, voyage);
+  }
 }

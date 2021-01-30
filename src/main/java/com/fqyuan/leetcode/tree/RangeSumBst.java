06ed26cb3a88a02f2ca5bceef3fbec0badd965f6
iap-git <alien.bigfool@gmail.com>
2021-01-31 00:04:13 +0800

    update on Jan 29th night 2021, easy rangeSumBst tree dfs problem.

---
+++ src/main/java/com/fqyuan/leetcode/tree/RangeSumBst.java
@@ -0,0 +1,24 @@
+package com.fqyuan.leetcode.tree;
+
+public class RangeSumBst {
+    /**
+     * https://leetcode.com/problems/range-sum-of-bst/
+     *
+     * @param root
+     * @param low
+     * @param high
+     * @return
+     */
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.val < low) {
+            return rangeSumBST(root.right, low, high);
+        }
+        if (root.val > high) {
+            return rangeSumBST(root.left, low, high);
+        }
+        return root.val + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
+    }
+}

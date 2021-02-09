d7d164c14a4073cffe72d8750f27c02dcdeff0e9
iap-git <alien.bigfool@gmail.com>
2021-02-09 23:48:27 +0800

    update on Feb 9th night 2021, bfs to calculate avg of level tree.

---
+++ src/main/java/com/fqyuan/leetcode/tree/LeafSimilar.java
@@ -0,0 +1,31 @@
+package com.fqyuan.leetcode.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeafSimilar {
+    /**
+     * https://leetcode.com/problems/leaf-similar-trees/ dfs 变形
+     *
+     * @param root1
+     * @param root2
+     * @return
+     */
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+        dfs(root1, list1);
+        dfs(root2, list2);
+        return list1.equals(list2);
+    }
+
+    private void dfs(TreeNode root, List<Integer> list) {
+        if (root != null) {
+            if (root.left == null && root.right == null) {
+                list.add(root.val);
+            }
+            dfs(root.left, list);
+            dfs(root.right, list);
+        }
+    }
+}

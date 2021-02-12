a73772a625724854df2c82f205ef8d550fc53780
iap-git <alien.bigfool@gmail.com>
2021-02-12 23:18:20 +0800

    update on Feb 12th night 2021, dfs to determine min difference.

---
+++ src/main/java/com/fqyuan/leetcode/tree/MinDifBst.java
@@ -0,0 +1,31 @@
+package com.fqyuan.leetcode.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MinDifBst {
+    /**
+     * https://leetcode.com/problems/minimum-absolute-difference-in-bst/
+     *
+     * @param root
+     * @return
+     */
+    public int getMinimumDifference(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        inOrder(root, list);
+        int result = Integer.MAX_VALUE;
+        for (int i = 1; i < list.size(); i++) {
+            result = Math.min(list.get(i) - list.get(i - 1), result);
+        }
+        return result;
+    }
+
+    private void inOrder(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        inOrder(root.left, list);
+        list.add(root.val);
+        inOrder(root.right, list);
+    }
+}

53d0103151980199aa4569eb5cb11ccd7a8a262a
iap-git <alien.bigfool@gmail.com>
2021-02-17 00:08:02 +0800

    update on Feb 16th night 2021, recursion of dfs.

---
+++ src/main/java/com/fqyuan/leetcode/tree/CousinTree.java
@@ -0,0 +1,36 @@
+package com.fqyuan.leetcode.tree;
+
+public class CousinTree {
+    /**
+     * https://leetcode.com/problems/cousins-in-binary-tree/
+     *
+     * @param root
+     * @param x
+     * @param y
+     * @return
+     */
+    TreeNode xParent = null;
+
+    TreeNode yParent = null;
+    int xDepth = -1, yDepth = -1;
+
+    public boolean isCousins(TreeNode root, int x, int y) {
+        getDepthAndParent(root, x, y, 0, null);
+        return xDepth == yDepth && xParent != yParent;
+    }
+
+    private void getDepthAndParent(TreeNode root, int x, int y, int depth, TreeNode parent) {
+        if (root == null) {
+            return;
+        }
+        if (root.val == x) {
+            xParent = parent;
+            xDepth = depth;
+        } else if (root.val == y) {
+            yParent = parent;
+            yDepth = depth;
+        }
+        getDepthAndParent(root.left, x, y, depth + 1, root);
+        getDepthAndParent(root.right, x, y, depth + 1, root);
+    }
+}

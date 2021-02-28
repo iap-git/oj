7bb8a0c29d3bc95cd9aa9e1277347591690976d9
iap-git <alien.bigfool@gmail.com>
2021-02-28 23:46:06 +0800

    update on March 2nd, todo.

--- src/main/java/com/fqyuan/leetcode/design/ToDoFeb27.java
+++ src/main/java/com/fqyuan/leetcode/design/ToDoFeb27.java
@@ -3,4 +3,5 @@ package com.fqyuan.leetcode.design;
 public class ToDoFeb27 {
     // TODO:
     // TODO: on Feb 28th
+    // TODO: on March 3rd
 }
--- src/main/java/com/fqyuan/leetcode/recursion/MinCostClimb.java
+++ src/main/java/com/fqyuan/leetcode/recursion/MinCostClimb.java
@@ -4,12 +4,63 @@ package com.fqyuan.leetcode.recursion;
  * @author fqyuan 2021/03/01
  */
 public class MinCostClimb {
+    public int minCostClimbingStairs1(int[] cost) {
+        int n = cost.length;
+        return Math.min(minCost1(cost, n - 1), minCost1(cost, n - 2));
+    }
 
-    public int minCostClimbingStairs(int[] cost) {
+    /**
+     * recursion
+     *
+     * @param cost
+     * @param n
+     * @return
+     */
+    private int minCost1(int[] cost, int n) {
+        if (n == 0 || n == 1) {
+            return cost[n];
+        }
+        return cost[n] + Math.min(minCost1(cost, n - 1), minCost1(cost, n - 2));
+    }
+
+    int[] dp;
+
+    public int minCostClimbingStairs2(int[] cost) {
+        int n = cost.length;
+        dp = new int[n];
+        return Math.min(minCost2(cost, n - 1), minCost2(cost, n - 2));
+    }
+
+    /**
+     * top-down dp
+     *
+     * @param cost
+     * @param n
+     * @return
+     */
+    private int minCost2(int[] cost, int n) {
+        if (n == 0 || n == 1) {
+            dp[n] = cost[n];
+        }
+        if (dp[n] != 0) {
+            return dp[n];
+        }
+        dp[n] = cost[n] + Math.min(minCost2(cost, n - 1), minCost2(cost, n - 2));
+        return dp[n];
+    }
+
+    public int minCostClimbingStairs3(int[] cost) {
         int n = cost.length;
         return Math.min(minCostItr(cost, n - 1), minCostItr(cost, n - 2));
     }
 
+    /**
+     * bottom up
+     *
+     * @param cost
+     * @param n
+     * @return
+     */
     private int minCostItr(int[] cost, int n) {
         if (n == 0 || n == 1) {
             return cost[n];
@@ -21,5 +72,5 @@ public class MinCostClimb {
             mem[i] = cost[i] + Math.min(mem[i - 1], mem[i - 2]);
         }
         return mem[n];
-    }
+  }
 }

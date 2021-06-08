918ef200667ef613d5abcc8ecdbf6430ea0162ed
iap-git <alien.bigfool@gmail.com>
2021-06-09 00:04:58 +0800

    update on June 8th, quickSort version 2.0

--- src/main/java/com/fqyuan/leetcode/sort/QuickSort.java
+++ src/main/java/com/fqyuan/leetcode/sort/QuickSort.java
@@ -41,4 +41,31 @@ public class QuickSort {
         qSort(nums, low, i - 1);
         qSort(nums, i + 1, high);
     }
+
+    public void quickSort(int[] arr, int low, int high) {
+        if (low >= high) {
+            return;
+        }
+        int i = low, j = high, index = arr[i];
+        while (i < j) {
+            while (i < j && arr[j] >= index) {
+                j--;
+            }
+            while (i < j && arr[i] <= index) {
+                i++;
+            }
+            swap(arr, i, j);
+        }
+        //set pivot to i
+        arr[low] = arr[i];
+        arr[i] = index;
+        quickSort(arr, low, i - 1);
+        quickSort(arr, i + 1, high);
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+  }
 }

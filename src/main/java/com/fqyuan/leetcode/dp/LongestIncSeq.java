39f8305537a76fe2d47a20aa20b200b341244202
iap-git <alien.bigfool@gmail.com>
2021-06-18 00:34:05 +0800

    update on June 18th, dynamic programming to get longest increasing sequence.

---
+++ src/main/java/com/fqyuan/leetcode/dp/LongestIncSeq.java
@@ -0,0 +1,27 @@
+package com.fqyuan.leetcode.dp;
+
+public class LongestIncSeq {
+
+    /**
+     * @param nums
+     * @return
+     */
+    public int lengthOfLIS(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int[] dp = new int[nums.length];
+        dp[0] = 1;
+        int ans = 1;
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            ans = Math.max(ans, dp[i]);
+        }
+        return ans;
+    }
+}

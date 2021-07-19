7cf288a9e241538ed6851af184c4e1e4308b1c34
iap-git <alien.bigfool@gmail.com>
2021-07-20 00:46:30 +0800

    update on July 19th, pre sum to get subArray sum.

---
+++ src/main/java/com/fqyuan/leetcode/queue/SubArraySum.java
@@ -0,0 +1,20 @@
+package com.fqyuan.leetcode.queue;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SubArraySum {
+    public int subarraySum(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int count = 0, preSum = 0;
+        for (int item : nums) {
+            preSum += item;
+            if (map.containsKey(preSum - k)) {
+                count++;
+            }
+            map.put(preSum, map.getOrDefault(preSum, 0) + 1);
+        }
+        return count;
+    }
+}

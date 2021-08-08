a320d2e7939821c5432c49962f1f69dada14b8ff
iap-git <alien.bigfool@gmail.com>
2021-08-09 00:31:22 +0800

    update on August 9th, review the double pointer trick.

---
+++ src/main/java/com/fqyuan/leetcode/twopointer/BianrySerach.java
@@ -0,0 +1,18 @@
+package com.fqyuan.leetcode.twopointer;
+
+public class BianrySerach {
+    int biSearch(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
